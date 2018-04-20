1cdd2246b3115c0e4ea905cf33bbb3a0d3ec93a9
Knabe <EKNABE1@volvocars.com>
2018-04-20 15:10:01 +0200

    Adding initial content to the monosim project

---
+++ monosim/Assets/Assets/Scripts/CameraPosTargetOrbit.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPosTargetOrbit : MonoBehaviour {
+
+    protected float _CameraDistance = 10f;
+    protected float _CameraX = 0;
+    protected float _CameraY = 0f;
+
+    public float ScrollSensitvity = 2f;
+    private float ScrollAmount = 0;
+
+    // Use this for initialization
+    void Start () {
+        _CameraDistance = -this.transform.localPosition.z;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        bool doUpdate = false;
+        
+        // Zooming Input from our Mouse Scroll Wheel
+        if (Input.GetMouseButton(1))
+        {
+            ScrollAmount = Input.GetAxis("Mouse Y") * 0.2f;
+            doUpdate = true;
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") != 0f)
+        {
+            ScrollAmount = Input.GetAxis("Mouse ScrollWheel") * ScrollSensitvity;
+            doUpdate = true;
+        }
+        if (doUpdate)
+        { 
+            ScrollAmount *= (this._CameraDistance * 0.3f);
+
+            this._CameraDistance += ScrollAmount;
+
+            this._CameraDistance = Mathf.Clamp(this._CameraDistance, 1.5f, 100f);
+            //Actual Camera Rig Transformations
+            this.transform.localPosition = new Vector3(this.transform.localPosition.x, this.transform.localPosition.y, -_CameraDistance);
+
+            print("Rotation orbit: " + this.transform.parent.localRotation.x + ", " + this.transform.parent.localRotation.y);
+        }
+    }
+}
---
+++ monosim/Assets/Assets/Scripts/follow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class follow : MonoBehaviour {
+
+    private Vector3 _oldPos;
+
+    public GameObject followObject;
+    public GameObject lookAt;
+    public float tension = 2.0f;
+    private float speed;
+
+    // Use this for initialization
+    void Start () {
+        _oldPos = this.transform.position;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 diffVec = followObject.transform.position - this.transform.position;
+        speed = (this.transform.position - _oldPos).magnitude / Time.deltaTime;
+        Vector3 diffUnitVec = diffVec.normalized;
+
+        float acceleration = tension * diffVec.magnitude - Mathf.Sqrt(2 * tension) * speed;
+
+        Vector3 newPos = this.transform.position + (speed + acceleration * Time.deltaTime) * diffUnitVec * Time.deltaTime;
+        _oldPos = newPos;
+
+        this.transform.position = newPos;
+        this.transform.LookAt(lookAt.transform);
+    }
+}
